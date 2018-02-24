f82418bba835eef2a3cb5539b2c715465b877dc3
Chiemeka Alim <lord.chiemy@gmail.com>
2018-02-24 22:09:25 +0100

    Modify builder to account for self referencing in closure table

--- builder.py
+++ builder.py
@@ -12,7 +12,7 @@ def xml_parser(root_tag):
     """
     Parses a tree, returning values for category and closure tables
     :param root_tag: The root element
-    :return: A tuple of category values and closure values
+    :return: A tuple of category values, closure values and closure self references
     """
     for child in root_tag:
         best_offer_enabled = child.find('{urn:ebay:apis:eBLBaseComponents}BestOfferEnabled')
@@ -32,22 +32,26 @@ def xml_parser(root_tag):
 
         closure_values = (category_values[4], category_values[0], category_values[2])
 
-        yield category_values, closure_values
+        closure_self_ref = (category_values[0], category_values[0], 0)
+
+        yield category_values, closure_values, closure_self_ref
 
 
 def db_insert(values):
     """
     Inserts values to database tables
-    :param values: A tuple of category values and closure values
+    :param values: A tuple of category values, closure values and closure self reference
     :return: None (implicitly)
     """
     category_val = values[0]
     closure_val = values[1]
+    closure_self_ref = values[2]
 
     # Try in-memory database https://stackoverflow.com/a/32239587/4333429
     # OR
     # use sql transactions https://stackoverflow.com/questions/5942402/python-csv-to-sqlite/7137270#7137270
     c.execute('INSERT INTO category VALUES (?, ?, ?, ?, ?, ?)', category_val)
+    c.execute('INSERT INTO category_closure VALUES (?, ?, ?)', closure_self_ref)
     c.execute('INSERT INTO category_closure VALUES (?, ?, ?)', closure_val)
 
 
