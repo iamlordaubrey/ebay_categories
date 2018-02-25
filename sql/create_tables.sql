9527286a27bce5478a527e141cc7e9657dc88016
Chiemeka Alim <lord.chiemy@gmail.com>
2018-02-25 08:17:54 +0100

    Change column data type to text

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -3,11 +3,11 @@ DROP TABLE IF EXISTS category_closure;
 
 CREATE TABLE category (
     category_id integer not null primary key,
-    category_name varchar(255),
+    category_name text,
     category_level integer,
-    best_offer_enabled varchar,
+    best_offer_enabled text,
     category_parent_id integer,
-    leaf_category varchar
+    leaf_category text
 );
 
 
